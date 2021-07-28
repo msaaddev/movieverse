d3fa575cb6b7f516987eda0eabcea70a4875eeeb
msaaddev <mrsaadirfan@gmail.com>
2021-07-28 18:01:42 +0500

    📦 NEW: SEO component

---
+++ components/SEO.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import config from '../utils/config';
+
+const SEO = () => {
+	return (
+		<Head>
+			<title>{config.title}</title>
+			<meta charSet="utf-8" />
+			<link rel="icon" href="/favicon.ico" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta name="description" content={config.description} />
+			<meta name="keywords" content={config.keywords} />
+			<meta property="og:locale" content="en_US" />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={config.title} />
+			<meta property="og:description" content={config.description} />
+		</Head>
+	);
+};
+
+export default SEO;
