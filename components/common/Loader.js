a3d90389c4e8ac77a3e862098d6088c04800c317
msaaddev <mrsaadirfan@gmail.com>
2021-07-28 23:40:36 +0500

    🐛 FIX: viewbox

--- components/common/Loader.js
+++ components/common/Loader.js
@@ -2,13 +2,8 @@ import ContentLoader from 'react-content-loader';
 
 const Loader = () => (
 	<div style={{ marginBottom: 50 }}>
-		<ContentLoader
-			speed={2}
-			width={'100%'}
-			height={300}
-			viewBox="0 0 100% 300"
-		>
-			<rect x="0" y="20" rx="2" ry="2" width={'100%'} height="300" />
+		<ContentLoader speed={2} width={300} height={300} viewBox="0 0 300 300">
+			<rect x="0" y="20" rx="2" ry="2" width="300" height="300" />
 		</ContentLoader>
 	</div>
 );
